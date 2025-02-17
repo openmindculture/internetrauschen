f0c892f7f2a66287f72e71b71a0442b2d1db2aba
Ingo Steinke <ingosteinke@gmx.de>
2025-02-17 19:56:00 +0100

    modular header footer includes

---
+++ inc/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Internetrauschen</title>
+    <meta charset="utf-8">
+    <style>
+        @font-face {
+            font-family: 'Adler';
+            src: url('/fonts/Adler.woff2') format('woff2');
+            font-weight: 400;
+            font-style: normal;
+            font-display: swap;
+        }
+        h1, h2, h3, h4, h5, h6 {
+            font-family: 'Adler', Helvetica, Verdana, sans-serif;
+            font-weight: 700;
+        }
+    </style>
+</head>
+<body>
+    <h1>Internetrauschen</h1>
