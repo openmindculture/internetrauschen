cb8bb0b8b33d7835aca53f4d4071077294bba50d
Ingo Steinke <ingosteinke@gmx.de>
2025-02-27 17:45:12 +0100

    add an HTML back and top link

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,8 @@
 <div class=".doubledotted"></div>
 <!-- auto-closing tag omission behavior of HTML parsing: --> <p class="outer"><p ="inner"> <!-- is equivalent to --> <p class="outer"></p><p class="inner"></p>
+<p class="outer"></p><p class="inner"></p>
+<!-- https://stackoverflow.com/questions/8814472/how-to-make-an-html-back-link -->
+<a href="javascript:history.back();return void(0);" title="an HTML back link">back</a>
+<a href="#top" title="an HTML top link">to the top</a>
 </body>
 </html>
