d32dc1618832c8b6503c9b3c58ae735832161598
Ingo Steinke <ingosteinke@gmx.de>
2025-02-27 17:46:49 +0100

    add void empty image just because

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,7 @@
 <!-- auto-closing tag omission behavior of HTML parsing: --> <p class="outer"><p ="inner"> <!-- is equivalent to --> <p class="outer"></p><p class="inner"></p>
 <p class="outer"></p><p class="inner"></p>
 <!-- https://stackoverflow.com/questions/8814472/how-to-make-an-html-back-link -->
-<a href="javascript:history.back();return void(0);" title="an HTML back link">back</a>
+<a href="javascript:history.back();return void(0);" title="an HTML back link">back <img src="" alt=""></img></a>
 <a href="#top" title="an HTML top link">to the top</a>
 </body>
 </html>
