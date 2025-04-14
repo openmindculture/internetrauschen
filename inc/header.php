01ecd637ea2c2d19b36ac93b6188b59a1bb7aadf
Ingo Steinke <ingosteinke@gmx.de>
2025-04-14 22:29:25 +0200

    add web fonts

--- inc/header.php
+++ inc/header.php
@@ -4,16 +4,39 @@
     <title>Internetrauschen</title>
     <meta charset="utf-8">
     <style>
+        /*! Adler is licensed under the 1001Fonts Free For Commercial Use License (FFC) */
         @font-face {
             font-family: 'Adler';
             src: url('/fonts/Adler.woff2') format('woff2');
+            font-weight: 700;
+            font-style: normal;
+            font-display: swap;
+        }
+        /*! Elizeth ExtraLight by Blackletra is licensed to Ingo Steinke */
+        @font-face {
+            font-family: 'Elizeth Extralight';
+            src: url('/fonts/ElizethExtralight.woff2') format('woff2');
             font-weight: 400;
             font-style: normal;
             font-display: swap;
         }
+        /*! Egory Castle by Seventh Imperium is licensed to Ingo Steinke */
+        @font-face {
+            font-family: 'Egory Castle Regular';
+            src: url('/fonts/egorycastle-webfont.woff2') format('woff2');
+            font-weight: 400;
+            font-style: normal;
+            font-display: swap;
+        }
+        body, * {
+            font-family: 'Elizeth Extralight', Times, serif;
+        }
         h1, h2, h3, h4, h5, h6 {
             font-family: 'Adler', Helvetica, Verdana, sans-serif;
             font-weight: 700;
+        }
+        .egorycastle {
+            font-family: 'Egory Castle Regular', 'egorycastle', medieval, serif;
         }
     </style>
 </head>
