4de24519ed30aa74fa05af6c2c8bd5faba75f7f4
Ingo Steinke <ingosteinke@gmx.de>
2025-05-08 10:22:00 +0200

    add text

--- index.php
+++ index.php
@@ -15,13 +15,24 @@
   Satzhappen <br>
   darbiete.<br>
     </strong></p>
+
+    <h3>Parodie der Ironie, dass selbst Suchmaschinen versagen</h3>
+
+    <p>Internetrauschen.de ist also auch eine Parodie auf die Ironie, dass Menschen — auch ohne KI — so viel Inhalt wie nie im Internet verbreiten — ernst oder lustig gemeint — die die sinnvollen Inhalte beiseite schieben und auffindbar machen, wenn selbst Suchmaschinen versagen, Wutschrei gegen generative KI, die durch noch mehr <a href="https://de.wikipedia.org/wiki/Bullshit">Bullshit</a> Menschen verwirrt und an den Rand dessen schiebt, das doch erschaffen wurde, um Menschen zu verbinden.</p>
+
+<h3>Unklares Kunstwerk gegen unklaren Unfung</h3>
+
+<p>Internetrauschen.de ist ein literarischen Gift, dass nach Gegengift ruft, ein Aufruf, diesen Unfug zu beenden, Schlussstriche zu ziehen, KI zu streichen, Marketing zu minimieren und endlich wieder mehr sinnvolles zu tun — es sei denn, der Unsinn sei — wie hier — gewollt und Kunst. Unklarheit und Unsicherheit der Kunst sind gesund, auch Zweifel fristet ein unberechtigtes Schattendasein im Zeitalter des Halbwissens nach dem Motto "Fake it until you make it". Schade!</p>
+
+<p>Zur Abwwechslung male mal ein Bild oder fotografiere eine Fotografie. Alles schwarzweiss auf dieser Seite. Es folgt ein Beispiel.</p>
+
 <h3 class="egorycastle">Bild des Tages</h3>
 <img alt="Fotografie verlorenen Ortes" src="img/lost_place_wikimedia_commons.jpg" width="960" height="540"/></img>
 <br>
 ^ Fotografie verlorenenen Ortes (rechtefrei vong Wikimedia Commons)
-    <div class="doppelgestrichen .doubledotted">
-    <!-- TODO hier auf andere Unterseiten verlinken beispielsweise gedicht-garten.php
-      -->
+    <div class="doppelgestrichen doppeltgestrichen .doubledotted">
+        &lt;!-- TODO hier auf andere Unterseiten verlinken -->
+
   <h2>Wortwolke</h2>
     <ul>
         <li><a href="#">Rausch</a></li>
