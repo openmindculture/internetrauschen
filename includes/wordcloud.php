e18a1dccc9ad841f2c378df487f25017db1ac192
Ingo Steinke <ingosteinke@gmx.de>
2025-05-16 14:26:14 +0200

    add notes

--- includes/wordcloud.php
+++ includes/wordcloud.php
@@ -3,3 +3,53 @@
 // but only internal links
 // inluding original term(s) if set
 // and some more or less random other 1 - 3 term combinations from terms array
+
+/*
+ * Internetrauschen
+NEXT finalize term randomization, internal linking, and add more pages,
+then add some inbound links on external pages and forums,
+only (much) later add generic external links to pinterest, ecosia, wikipedia, google
+then keep testing, tuning, extending, before announcing in a blog post eventually
+
+validate or esc term
+but not validate against list
+then we have option for true infinity
+
+shorten terms list for outlinks and related internals
+
+split terms list to $term, $term2, $term3 to replace placeholders
+and if empty, add randomly from words array
+
+place some inbound links here and there
+add some better poetry centered zine pages
+add some serious topical pages like gender pay gap
+add some more fixed content hobbyist pages like typical blog posts
+food travel personal experience and also add some potential "first-hand" cues
+
+then add an initial terms list and click around to explore,
+note missing pages that would make sense, to fill as fixed pages,
+then take a set of pages and ask chat gpt to make similar paragraphs...
+and feed those into a generic text block library
+where we will replace a handful of subject/object in sentences with $term...
+introduce some slight variations
+and shuffle paragraphs
+
+repeat explorative clicking and noting some pages to rewrite with own text
+
+finally update the list of available fixed pages
+and feature some of them within fixed text on other pages
+and then place some more external links to such sub pages
+
+hash map  of existing files
+to prioritize in "related articles"
+title as key, file name as value
+so that we can make aliases
+and (iterate (quicker?)) for partial key matches
+or else first try for a direct match and only fall back to a partial unless no direct
+
+gender pay gap: geschlechter-einkommenskluft geschlecht - einkommen - kluft
+kaukasisch armut einkommen kluft geschlecht
+Einträge variieren mit Bindestrichen oder nur Leerzeichen
+kaukasisch kreide kreis
+intelligent reich privilegiert
+ */
---
+++ unwort-barrierearm-nachhaltig.php
@@ -0,0 +1,23 @@
+Unwort, barrierearm, nachhaltig
+
+Barrierearm als Begriff aus dem Accessibility- bzw. Sozialkontext bleibt vage, problematisch und entbehrt einer Entsprechung in der englischen Sprache. Alle drei Begriffe sind stark kontextabhängig und werden oft inflationär oder ungenau gebraucht. Besonders in der Werbe- oder Agentursprache. Ton: Ironisch bis kritisch. Betont den Trend zur inhaltsleeren „Buzzwordisierung“. Das Unwort barrierearm ist oft eine weichere oder unpraezise Alternative zu barrierefrei. Barrierearm ist ein schwammiger Begriff, der gern als Euphemismus verwendet wird.
+
+<h2>Ist Armut ein Unwort?</h2>
+
+Aber es gibt es auch Worte wie Blutarmut, Sauerstoffarmut oder Wortarmut gibt.
+
+Assoziationen mit dem Wort Armut beinhalten Arbeitslosigkeit, Angewisensein, Not, Scham, Elend und Diskriminierung durch Ungerechtigkeit, Verschwendung und Reichtum, aber auch Bescheidenheit und bewussten Verzicht. Mit nachhaltig verbinden sich Begriffe wie Bauen, Energieversorgung, Umweltschutz, Forstwirtschaft, Entwicklung, Lebensstil, Akteure, Innovation, Reduzierung, Verbesserung, Erhaltung, Optimierung, Akzeptanz, Investition, Langfristigkeit und Effizienz.
+
+Sind solche Begriffe eine Armutszeugnis unserer Zeit?
+
+<h2>Ist Unwort ein Unwort?</h2>
+
+Es gibt keine Assoziation gefunden für das Wort Unwort. Ist Unwort ein Unwort ist?
+
+Schon das Deutsche Woertrebuch der Brueder Grimm verzeichnet Unwort als boeses, beleidigendes, verletzendes, verwerfliches Wort. Neben dem Unwort des Jahres wird jährlich auch das Wort des Jahres von einer Jury festgelegt. Unwortvorschläge werden jederzeit von allen Menschen schriftlich und unter Angabe der Quelle eingesandt werden. Damit soll auf sprachliche Missgriffe in den Medien und der Politik aufmerksam gemacht und dadurch das Bewusstsein und die Sensibilität für Sprache gefördert werden. Ziel der Unwort-Aktion ist eine kritische Auseinandersetzung mit dem Sprachgebrauch in der öffentlichen Kommunikation. Biodeutsch ist Unwort des Jahres 2024. Des Weiteren gibt es noch das Jugendwort des Jahres, das vielleicht am meisten kritisierte Wort ist.
+
+<h2>Ist nachhaltig ein Unwort?</h2>
+
+Nachhaltigkeit ist ein Modebegriff geworden und wurde mehrfach erfolglos als Unwort des Jahres vorgeschlagen.
+
+Worum es bei diesem wichtigen Wort wirklich geht, wissen wohl wenige. Nachhaltigkeit ist mehr als Klimaschutz. Als die drei Dimensionen der nachhaltigen Entwicklung geht von der Vorstellung aus, dass nachhaltige Entwicklung nur durch das gleichzeitige und gleichberechtigte Umsetzen von umweltbezogenen, wirtschaftlichen und sozialen Zielen erreicht werden kann. Die dreidimensionale Definition ist aber nicht nur ein Wort, sondern ein Konzept. Ein zweifelsohne wichtiges Thema, das ist klar. Doch selten hat ein Begriff mit so einer langen Geschichte so viel Kritik als Modeunwort auf sich gezogen wie nachhaltig. Nachhaltigkeit in der Forstwirtschaft bezeichnet die forstliche Praxis der Nachhaltigkeit, also ein Handlungsprinzip zur Ressourcen-Nutzung. Dieses forstwirtschaftliche Handlungsprinzip wurde erstmals für die sich zu Beginn des 18. Jahrhunderts formuliert.
