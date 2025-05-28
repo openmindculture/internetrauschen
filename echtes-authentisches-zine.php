d8e9f093c12cd3b008e5a5bd3d32afea4ea5ae68
Ingo Steinke <ingosteinke@gmx.de>
2025-05-28 10:31:11 +0200

    dd even more content

---
+++ echtes-authentisches-zine.php
@@ -0,0 +1,27 @@
+<h1>Echtes Authentisches Zine</h1>
+Berlin, Ende Mai 2025<br>
+<p>Internetrauschen wird echt zum authentischen Independent-Zine, um das mal so zu sagen.<br>
+    Nur damit keine Zweifel aufkommen. Rau und ehrlich vom Stil her, handgeschrieben.<br>
+    Kurz, lakonisch, wie bei Rocko Schmaoni, als er in seinen Jugendjahren in Norddeutschland ein Punk-Fanzine geschrieben hat.<br>
+    Das berichtet er in seinem Erinnerungen, die mit dem Titel <a
+        href="https://de.wikipedia.org/wiki/Dorfpunks" target="_blank">Dorfpunks als Buch</a> herausgegeben wurden.<br>
+    Weil er keine Bands kannte, war ein Teil des ganzen frei erfunden. Aber alle hatten Spass.<br>
+</p>
+<h2>Alle hatten Spass</h2>
+<p>So soll es beim Internetrauschen auch zugehen. Alle haben Spass! Ich zumindestens. Alles echt und ehrlich. Echt jetzt!
+Deswegen habe ich auch das Datum oben hin geschrieben. Sicherheitshalber. Details sind wichtig.<br>
+    Sagt <a href="https://frankgoosen.de/" target="_blank">Frank Goosen</a> ja auch: "Details sind wichtig."<br>
+    und ein paar Links raus zu Seiten, die schon als ehrlich etabliert sind.<br>
+    Vielleicht verlinken sie ja auch mal zu dieser hier.<br>
+    Waere doch mal was!<br>
+    <br>
+</p>
+
+Bildnachweis: die Bilder sind auch fast alle von mir selbst gemacht, fotografiert und mit ordentlich kontrast in
+Schwarzweiss hochgeladen geworden. Abgesehen von einigen, die ich von Wikipedia kopiert habe, was aber erlaubt wurde.<br>
+Die habe ich entsprechend gekennzeichnet.<br>
+So viel dazu.<br>
+<br>
+Hier ist ein Bild von einem Bleistift, einem Anspitzer und von dem Abrieb, der vom Anspitzen bleibt.
+<img alt="Fotografie von einem angespitzten Bleistift" src="img/bleistift-spitzer-abrieb.jpg" width="960" height="540"/></img>
+<br>
