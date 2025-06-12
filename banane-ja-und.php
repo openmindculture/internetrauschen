4ff5181c3f9787bb383cc56fdbd391810aece84d
Ingo Steinke <ingosteinke@gmx.de>
2025-06-12 11:53:33 +0200

    add content

---
+++ banane-ja-und.php
@@ -0,0 +1,48 @@
+<?php global /* @var {String} */ $get_the_title; ?>
+<?php $get_the_title = 'Banane, ja und?'; ?>
+<?php include_once('includes/header.php') ?>
+<h1>Banane, ja und?</h1>
+    <p>Ja, und? bedeutet auf Deutsch etwa das Gegenteil des positiven englischen Ausdrucks "yes, and..." der im Impro-
+Theater so beliebte Banane-Musik. Das deutsche Wort Banane bezeichnet eine Frucht, die hinter dem Namen des
+Impro-Theaters steht. Aber in einer anderen Sprache bedeutet Banane etwas anderes, etwa so viel wie "Ja und?".
+
+    <p>Bananen gehören zur Familie der Musaceae und stammen aus Asien, sind heute aber weltweit in tropischen und
+    subtropischen oder mediterranen Regionen verbreitet, wie zum Beispiel auf Mallorca oder in Split.
+
+    <p>Eine Banane auf einem Foto sieht so aus.<br>
+    <br>
+    <br>
+
+    <img alt="Fotografie einer Banane" src="img/banane.jpg" width="960" height="540"/></img>
+    <br>
+
+    <h2>Geschmackssache: Banana Split, Bananentrunk und andere Bananenrezepte</h2>
+    <p>Es gibt viele Banenensorten,
+        zum Beispiel die Desserbananen, aber es gibt auch Kochbananen, welche eher wie Kartoffeln verwendet werden.
+        Kochbananen sind eine Bananenart, die sich gut zum Kochen eignet. Bananen ohne den Hinweis auf biologische
+        Bedeutung sind oft als konventionelle Bananen bezeichnet und sie sind oft mit giftigen Pestiziden belastet.
+
+    <p>Banenwasser gilt als Schlaftrunk. Ein einfaches Rezept für Bananenwasser ist, eine Banane mit Schale in Scheiben zu
+        schneiden, mit kochendem Wasser zu begiessen und ziehen zu lassen. Das Bananenwasser kann dann abgeseiht und warm
+        getrunken werden, oft wird auch eine Prise Zimt zugegeben.
+
+    <p>Zugegeben ist Banentrunk Geschmackssache und schmeckt nicht jedem. Bananen lassen sich auch braten. Dann schmecken
+        sie gleich viel besser. Wer es mag, kann ganz viel Fett, Zucker und Honig hinzugeben. Aus Bananen lassen sich auch
+        Speiseeisdesserts zubereiten, zum Beispiel das beliebte Bananen-Eis "Banana Split".
+
+    <p>Mit Bananen kann Bananenbrot gebacken oder Bananen lassen sich oder als Topping von Eierkuchen verwenden.
+        Weitere leckere Bananenrezepte sind schnelle Schoko-Bananen-Muffins, Bananen-Walnuss-Cookies, Schoko-Bananenbrot mit
+        Joghurt oder Bananen-Curry auf seychellische Art.
+
+    <h2>Was ist eine Banane eigentlich?</h2>
+
+    Eine Banane ist eine lange, meist leicht gebogene Frucht mit einer weichen Schale, die gelb wird, wenn sie reif ist.
+    Eine Banane hat weiches Fruchtfleisch im Inneren. Der tropische Bananenbaum, an dem die Bananen wachsen,
+    ist, wissenschaftlich betrachtet, gar kein Baum, sondern ein Kraut. Das hat eine wissenschaftliche Studie
+    amerikanischer Wissenschaftler bewiesen, habe ich gelesen.
+
+
+
+
+
+<?php include_once('includes/footer.php') ?>
