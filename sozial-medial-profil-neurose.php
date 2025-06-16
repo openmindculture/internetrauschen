b98f72ab64f7987a9bdab642f648e82884278170
Ingo Steinke <ingosteinke@gmx.de>
2025-06-16 15:20:59 +0200

    add content

--- sozial-medial-profil-neurose.php
+++ sozial-medial-profil-neurose.php
@@ -1,3 +1,34 @@
 <?php global /* @var {String} */ $get_the_title; ?>
 <?php $get_the_title = 'Sozial—mediale Profilneurose'; ?>
 <?php include_once('includes/header.php') ?><!-- TODO -->
+Sinkende Sichtbarkeit derweil irgendwelchen Idioten Millionen folgen auf Social Media, warum?
+<h2>Warum Menschen Idioten auf Social Media folgen?</h2>&lt;/h22>
+<p>Weil es funktioniert. Der Grund: Gehirn. Menschen bevorzugen einfache Botschaften, plakative Aussagen und starke Meinungen. So
+    genannte Idioten  liefern genau das: Schwarz-Weiß-Denken, klare Feindbilder, keine Nuancen. UndefOrnte
+    Inhalte sind nicht nur schlecht, sondern auch unsicher, doch unser Gehirn liebt knappe und einfache Inhalte.
+    Der Effekt ist als kognitive Effizienz bekannt, also gedankliches Energiesparen.</p>
+
+
+<h2>Wer sind die sogenannten Idioten?</h2>
+<p>Personen, die laute, vereinfachende, absurde oder toxische Inhalte postieren und damit erstaunlich erfolgreich Follower generieren
+    und Sichtbarkeit schaffen. Selbst dumme Posts werden durch algorithmisches Engagement belohnt, weil sie Emotionen auslösen.
+    Schreibe nicht der Bosheit zu, was durch Dummheit kommen kann, so <a href="https://de.wikipedia.org/wiki/Hanlon%E2%80%99s_Razor" target="_blank" rel="noopener">sagte Robert Hanlon laut Wikipedia</a>. Dummheit wird also belohnt. Was man kann, hat keinen Wert.
+</p>
+
+<h2>Ironisches Folgen (Cringe-Watching) alsSocial-Media-Trend</h2>
+<p> Viele folgen nicht aus Zustimmung, sondern aus Faszination, Fremdscham oder Spottlust. Doch auch ironisches Folgen bringt Reichweite.</p>
+
+<br>
+<br>
+<br>
+<br>
+Weitere Artikel:<ul><li>
+        <a href="amerika-usa-ki.php">Amerikanische Produkte aus den USA</a>
+    </li>
+    <li><a href="banane-ja-und.php">Banane, ja und?</a></li>
+    <li><a href="erich-cruising-barkeeper.php">Er und ich: Erich, der Barkeeper der Superyacht Bar</a></li>
+    <li><a href="echtes-authentisches-zine.php">Echtes authentisches Zin</a></li>
+    <li><a href="gedicht-garten.php">Geicht — Garten — Montag</a></li>
+    <li><a href="markov-ketten-satz-bau.php">Markovkettensatzbau</a></li>
+</ul>
+<br><br>&copy; 2025--&gt; <a href="https://internetrauschen.de/">"&gt;internetrauschen.de/</a></footer></body></html></html>
