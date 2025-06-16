041ac4cca8cb78106afa740f7781672e2257bb39
Ingo Steinke <ingosteinke@gmx.de>
2025-06-16 12:47:50 +0200

    linked list

--- sitemap.xml.php
+++ sitemap.xml.php
@@ -1,20 +1,20 @@
-<?php ?><html><head><title>Internetrauschen Sitemap</title></head><body><pre><?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<url><loc>https://internetrauschen.de/</loc></url>
-<url><loc>https://internetrauschen.de/altbaucharme-brathering-urinstinkt.php</loc></url>
-<url><loc>https://internetrauschen.de/amerika-usa-ki.php</loc></url>
-<url><loc>https://internetrauschen.de/abreit-reise-tage.php</loc></url>
-<url><loc>https://internetrauschen.de/banane-ja-und.php</loc></url>
-<url><loc>https://internetrauschen.de/dienstag-gleis-dreieck.php</loc></url>
-<url><loc>https://internetrauschen.de/echtes-authentisches-zine.php</loc></url>
-<url><loc>https://internetrauschen.de/erich-cruising-barkeeper.php</loc></url>
-<url><loc>https://internetrauschen.de/gedicht-garten.php</loc></url>
-<url><loc>https://internetrauschen.de/ki-schweine-schwall-slop-kontern.php</loc></url>
-<url><loc>https://internetrauschen.de/markov-ketten-satz-bau.php</loc></url>
-<url><loc>https://internetrauschen.de/sitemap.xml</loc></url>
-<url><loc>https://internetrauschen.de/sitemap.xml.php</loc></url>
-<url><loc>https://internetrauschen.de/sozial-medial-profil-neurose.php</loc></url>
-<url><loc>https://internetrauschen.de/spam-leiden-handeln.php</loc></url>
-<url><loc>https://internetrauschen.de/unwort-barrierearm-nachhaltig.php</loc></url>
+<?php ?><html><head><title>Internetrauschen Sitemap</title></head><body><h1>Internetrauschen Sitemap</h1>
+<pre><urlset>
+<url><a href="https://internetrauschen.de/">https://internetrauschen.de/</a></url>
+<url><a href="https://internetrauschen.de/altbaucharme-brathering-urinstinkt.php">https://internetrauschen.de/altbaucharme-brathering-urinstinkt.php</a></url>
+<url><a href="https://internetrauschen.de/amerika-usa-ki.php">https://internetrauschen.de/amerika-usa-ki.php</a></url>
+<url><a href="https://internetrauschen.de/abreit-reise-tage.php">https://internetrauschen.de/abreit-reise-tage.php</a></url>
+<url><a href="https://internetrauschen.de/banane-ja-und.php">https://internetrauschen.de/banane-ja-und.php</a></url>
+<url><a href="https://internetrauschen.de/dienstag-gleis-dreieck.php">https://internetrauschen.de/dienstag-gleis-dreieck.php</a></url>
+<url><a href="https://internetrauschen.de/echtes-authentisches-zine.php">https://internetrauschen.de/echtes-authentisches-zine.php</a></url>
+<url><a href="https://internetrauschen.de/erich-cruising-barkeeper.php">https://internetrauschen.de/erich-cruising-barkeeper.php</a></url>
+<url><a href="https://internetrauschen.de/gedicht-garten.php">https://internetrauschen.de/gedicht-garten.php</a></url>
+<url><a href="https://internetrauschen.de/ki-schweine-schwall-slop-kontern.php">https://internetrauschen.de/ki-schweine-schwall-slop-kontern.php</a></url>
+<url><a href="https://internetrauschen.de/markov-ketten-satz-bau.php">https://internetrauschen.de/markov-ketten-satz-bau.php</a></url>
+<url><a href="https://internetrauschen.de/sitemap.xml">https://internetrauschen.de/sitemap.xml</a></url>
+<url><a href="https://internetrauschen.de/sitemap.xml.php">https://internetrauschen.de/sitemap.xml.php</a></url>
+<url><a href="https://internetrauschen.de/sozial-medial-profil-neurose.php">https://internetrauschen.de/sozial-medial-profil-neurose.php</a></url>
+<url><a href="https://internetrauschen.de/spam-leiden-handeln.php">https://internetrauschen.de/spam-leiden-handeln.php</a></url>
+<url><a href="https://internetrauschen.de/unwort-barrierearm-nachhaltig.php">https://internetrauschen.de/unwort-barrierearm-nachhaltig.php</a></url>
     <?php /* TODO add random content */ ?>
 </urlset></pre></body></html>
