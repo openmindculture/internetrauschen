08b5c7b5ceea05d2dd53442a8b3245ee6dda02f1
Ingo Steinke <ingosteinke@gmx.de>
2025-07-15 16:42:26 +0200

    add home link everywhere

--- includes/footer.php
+++ includes/footer.php
@@ -2,3 +2,4 @@
 <footer>
 <a href="javascript:history.back();return void(0);" title="an HTML back link">back <img src="" alt=""></img></a>
 <a href="#top" title="an HTML top link">an die Spitze (to the top)</a>
+oder <a href="index.php" title="zur Startseite">geh heim</a>
