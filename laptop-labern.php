82a17aa4988150325d2d17dcf97702c7efa715b2
Ingo Steinke <ingosteinke@gmx.de>
2025-07-15 17:34:21 +0200

    improve layout

--- laptop-labern.php
+++ laptop-labern.php
@@ -5,7 +5,7 @@ $get_the_title; ?>
 
     <p>Das ist das moderne Leben: tippen, telefonieren, Mausrad statt Fließband, Laptop statt Schmiedehammer. Kein Wunder, dass die Menschen sich immer mehr wie Roboter fühlen und fürchten, von solchen ersetzt zu werden. Dabei gibt es genug zu tun, das uns die künstlichen Kollegen noch lange nicht abnehmen: Serien sehen und Tik Tok tanzen zum Beispiel, oder Handwerk, Landwirtschaft und Kinder erziehen. Damit die Kinder auch Tik Tonk Tanzen lernen und später, wenn sie groß sind, auch am Laptop tippen und Serien sehen, um ihr eigenes Geld zu verdienen.</p>
 
-    <img alt="Fotografie eines modernen Schriebtischarbeitsplatzes" src="img/maus-schreibtisch.jpg" width="960" height="540"/></img>
+
 
     <h2 class="egorycastle">Alltag in der Moderne (Gedicht)</h2>
 
@@ -22,5 +22,8 @@ $get_the_title; ?>
     Sleep<br>
     Repeat<br>
     <br>
+<br>
+    <img alt="Fotografie eines modernen Schriebtischarbeitsplatzes" src="img/maus-schreibtisch.jpg" width="960" height="540"/></img>
+<br><br>
 
 <?php include_once('includes/footer.php') ?>
