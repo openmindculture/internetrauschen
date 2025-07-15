0aae755264e22c6641b6eefc87fe8b0c90fb4975
Ingo Steinke <ingosteinke@gmx.de>
2025-07-15 17:30:36 +0200

    add another poem post

---
+++ laptop-labern.php
@@ -0,0 +1,26 @@
+<?php global /* @var {String} */
+$get_the_title; ?>
+<?php $get_the_title = 'Laptop, labern, ...'; ?>
+<?php include_once('includes/header.php') ?>
+
+    <p>Das ist das moderne Leben: tippen, telefonieren, Mausrad statt Fließband, Laptop statt Schmiedehammer. Kein Wunder, dass die Menschen sich immer mehr wie Roboter fühlen und fürchten, von solchen ersetzt zu werden. Dabei gibt es genug zu tun, das uns die künstlichen Kollegen noch lange nicht abnehmen: Serien sehen und Tik Tok tanzen zum Beispiel, oder Handwerk, Landwirtschaft und Kinder erziehen. Damit die Kinder auch Tik Tonk Tanzen lernen und später, wenn sie groß sind, auch am Laptop tippen und Serien sehen, um ihr eigenes Geld zu verdienen.</p>
+
+    <img alt="Fotografie eines modernen Schriebtischarbeitsplatzes" src="img/maus-schreibtisch.jpg" width="960" height="540"/></img>
+
+    <h2 class="egorycastle">Alltag in der Moderne (Gedicht)</h2>
+
+    Laptop,<br>
+    Labern,<br>
+    Tippen,<br>
+    Telefonieren<br>
+    Meeting,<br>
+    Greeting,<br>
+    Heim geh´n<br>
+    Serien seh´n<br>
+    Tik Tok Tanzen<br>
+    Eat<br>
+    Sleep<br>
+    Repeat<br>
+    <br>
+
+<?php include_once('includes/footer.php') ?>
